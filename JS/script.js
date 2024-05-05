4faba1b6000631063e079ec61f2f94a8ad289339
Jazael Perez <131943183+perezjazaelr@users.noreply.github.com>
2024-05-06 03:54:58 +0800

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,147 @@
+const toggleButon = document.getElementsByClassName('toggle-button')[0]
+			const navbarLinks = document.getElementsByClassName('sidebar')[0]
+
+			toggleButon.addEventListener('click', () => {
+				navbarLinks.classList.toggle('active')
+				navbarLinks.classList.add('link--transition')
+        })
+
+
+
+    function My_INTRODUCTION(){
+        location.href="../HTML/PHP-INTRO.php"
+    }
+    function My_OPERATOR(){
+        location.href="../HTML/OPERATOR.php"
+    }
+    function My_CONTROLSTATE(){
+        location.href="../HTML/CONTROL.php"
+    }
+
+
+
+
+    function Myland(){
+        location.href="../HTML/landpage.php"
+    }
+    function Myhome(){
+        location.href="../HTML/INTRO-hello.php"
+    }
+    function Mycomment(){
+        location.href="../HTML/INTRO-comments.php"
+    }
+    function Myvariables(){
+        location.href="../HTML/INTRO-Variables.php"
+    }
+    function Myecho(){
+        location.href="../HTML/INTRO-echo.php"
+    }
+    function MyvarDump(){
+        location.href="../HTML/INTRO-VarDump.php"
+    }
+    function My$var(){
+        location.href="../HTML/INTRO-$and$$.php"
+    }
+    function Mycons(){
+        location.href="../HTML/INTRO-Constant.php"
+    }
+    function MymagCons(){
+        location.href="../HTML/INTRO-magicCons.php"
+    }
+    function Mydata(){
+        location.href="../HTML/INTRO-DataTypes.php"
+    }
+    function MytypeCast(){
+        location.href="../HTML/INTRO-typeCast.php"
+    }
+    function MytypeJug(){
+        location.href="../HTML/INTRO-typeJug.php"
+    }
+    function Mystrings(){
+        location.href="../HTML/INTRO-String.php"
+    }
+    function MyBoolean(){
+        location.href="../HTML/INTRO-boolean.php"
+    }
+    function Myint(){
+        location.href="../HTML/INTRO-int.php"
+    }
+    function Myfiles(){
+        location.href="../HTML/INTRO-files.php"
+    }
+    function Mymaths(){
+        location.href="../HTML/INTRO-math.php"
+    }
+    function Myhere(){
+        location.href="../HTML/INTRO-here.php"
+    }
+    function Mycomp(){
+        location.href="../HTML/INTRO-CompTypes.php"
+    }
+    
+    
+    
+    function Myopera(){
+        location.href="../HTML/OPER-operator.php"
+    }
+    function Myarit(){
+        location.href="../HTML/OPER-arith.php"
+    }
+    function Mycompa(){
+        location.href="../HTML/OPER-compOper.php"
+    }
+    function Mylogical(){
+        location.href="../HTML/OPER-logOper.php"
+    }
+    function Myass(){
+        location.href="../HTML/OPER-assOper.php"
+    }
+    function Mystring(){
+        location.href="../HTML/OPER-stringOper.php"
+    }
+    function Myarr(){
+        location.href="../HTML/OPER-arrayOper.php"
+    }
+    function Mycondi(){
+        location.href="../HTML/OPER-condiOper.php"
+    }
+    function Myspread(){
+        location.href="../HTML/OPER-spreadOper.php"
+    }
+    function Mynull(){
+        location.href="../HTML/OPER-nullOper.php"
+    }
+    function Myspace(){
+        location.href="../HTML/OPER-SpaceOper.php"
+    }
+    
+    
+    
+    function Mydeci(){
+        location.href="../HTML/CONT-Desci.php"
+    }
+    function Myifelse(){
+        location.href="../HTML/CONT-ifelse.php"
+    }
+    function Myswitch(){
+        location.href="../HTML/CONT-Switch.php"
+    }
+    function MyforLoop(){
+        location.href="../HTML/CONT-forLoop.php"
+    }
+    function MyforeachLoop(){
+        location.href="../HTML/CONT-foreachLoop.php"
+    }
+    function MywhileLoop(){
+        location.href="../HTML/CONT-whileLoop.php"
+    }
+    function MydowhileLoop(){
+        location.href="../HTML/CONT-dowhileLoop.php"
+    }
+    function Mybreak(){
+        location.href="../HTML/CONT-breakStatement.php"
+    }
+    function MyConti(){
+        location.href="../HTML/CONT-continueStatement.php"
+    }
+    
